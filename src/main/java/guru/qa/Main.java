fffde2b7a9ce3c5bf0855a858f5862c6de01ba18
vy4cheslav <d4m1enpwnz@gmail.com>
2023-12-23 17:57:36 +0300

    add something

--- src/main/java/guru/qa/Main.java
+++ src/main/java/guru/qa/Main.java
@@ -1,11 +1,8 @@
 package guru.qa;
 
 public class Main {
-    private static String msg = "Hello qa.guru!!!!!";
+    private static String msg = ""Hello qa.guru!!!!!"";
     public static void main(String[] args) {
-        System.out.println(getMessage());
-    }
-    private static String getMessage(){
-        return "Hello qa.guru!";
+        System.out.println("Hello qa.guru!");
     }
 }
